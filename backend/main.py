99a27c6444081a51976cb88963e753127ed539ce
user <solomonboltin@gmail.com>
2025-06-22 15:54:51 +0300

    Update CORS middleware to allow all origins temporarily

--- backend/main.py
+++ backend/main.py
@@ -56,7 +56,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], # Allows all origins for CORS todo - restrict this in production
     allow_credentials=True,
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
