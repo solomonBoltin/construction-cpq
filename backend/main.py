43c9bd71b643d0f5378017d11aa657156adce9aa
user <solomonboltin@gmail.com>
2025-07-01 19:19:45 +0300

    feat: add exception handling middleware to improve error management, fixing cors error on 500

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 import logging
@@ -53,6 +53,16 @@ app = FastAPI(
     version="0.1.0",
     lifespan=lifespan
 )
+
+async def catch_exceptions_middleware(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception:
+        # you probably want some kind of logging here
+        return Response("Internal server error", status_code=500)
+
+
+app.middleware('http')(catch_exceptions_middleware)
 
 app.add_middleware(
     CORSMiddleware,
